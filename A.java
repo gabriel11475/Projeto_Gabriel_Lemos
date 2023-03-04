f195175834b3c7965a4642b9884a8a62e97b08fe
gabriel11475 <116923636+gabriel11475@users.noreply.github.com>
2023-03-04 08:32:03 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,28 @@
+public class A {
+    int A1;
+    float A2;
+
+    public int getA1() {
+        return A1;
+    }
+
+    public double getA2() {
+        return A2;
+    }
+
+    public void setaA1(int x) {
+        A1 = x;
+    }
+
+    public void setA2(float x) {
+        A2 = x;
+    }
+
+    public void MA1() {
+        System.out.println("MA1");
+    }
+
+    public void MA2() {
+        System.out.println("MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,28 @@
+public class B {
+    int B1;
+    float B2;
+
+    public int getB1() {
+        return B1;
+    }
+
+    public double getB2() {
+        return B2;
+    }
+
+    public void setB1(int x) {
+        B1 = x;
+    }
+
+    public void setB2(float x) {
+        B2 = x;
+    }
+
+    public void MB1() {
+        System.out.println("MB1");
+    }
+
+    public void MA2() {
+        System.out.println("MB2");
+    }
+}
