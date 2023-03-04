e61d9685e289086d5cada714468f0fa4d47a38d7
gabriel11475 <116923636+gabriel11475@users.noreply.github.com>
2023-03-04 14:25:46 -0300

    teste

--- D.java
+++ D.java
@@ -6,4 +6,12 @@ public class D {
     public void MD2() {
         System.out.println("MD2");
     }
+    
+    public void MB3() {
+        System.out.println("MB3");
+    }
+    
+    public void MB4() {
+        System.out.println("MB4");
+    }
 }
